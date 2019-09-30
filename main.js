3ed4bb7e728ceaba6fee16fe5afc4824e027dc78
ChanJun-Park <pcj01142@gmail.com>
2019-09-30 15:19:44 +0900

    nodejs에서 파일리스트 출력

--- main.js
+++ main.js
@@ -2,23 +2,8 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url,true).query;
-    var pathname = url.parse(_url, true).pathname;
-
-    if (pathname === '/') {
-      if(queryData.id === undefined) {
-        // 홈화면
-        fs.readdir('data', function(err, files) {
-          var list = '<ul>';
-          for (var i = 0; i < files.length; i++) {
-            list += `<li><a href='/?id=${files[i]}'>${files[i]}</a></li>`;
-          }
-          list += '</ul>';
-          var title = 'Welcome';
-          var description = 'Hello Node.js';
-          var template = `
+function templateHTML(title, list, body) {
+  var template = `
         <!doctype html>
         <html>
         <head>
@@ -29,43 +14,46 @@ var app = http.createServer(function(request,response){
           <h1><a href="/">Web</a></h1>
           ${list}
           <h2>${title}</h2>
-          <p>
-          ${description}
-          </p>
+          ${body}
         </body>
         </html>
         `;
-        response.writeHead(200);
-        response.end(template);
+  return template;
+}
+
+function templateList(files) {
+  var list = '<ul>';
+  for (var i = 0; i < files.length; i++) {
+    list += `<li><a href='/?id=${files[i]}'>${files[i]}</a></li>`;
+  }
+  list += '</ul>';
+  return list;
+}
+
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url,true).query;
+    var pathname = url.parse(_url, true).pathname;
+
+    if (pathname === '/') {
+      if(queryData.id === undefined) {
+        // 홈화면
+        fs.readdir('data', function(err, files) {
+          var list = templateList(files);
+          var title = 'Welcome';
+          var description = 'Hello Node.js';
+          var template = templateHTML(title, list, `<p>${description}</p>`);
+          response.writeHead(200);
+          response.end(template);
         });
       } else {
         fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description) {
           fs.readdir('data', function(err, files){
-            var list = '<ul>';
-            for (var i = 0; i < files.length; i++) {
-              list += `<li><a href='/?id=${files[i]}'>${files[i]}</a></li>`;
-            }
-            list += '</ul>';
+            var list = templateList(files);
             var title = queryData.id;
-            var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">Web</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>
-            ${description}
-            </p>
-          </body>
-          </html>
-          `;
-          response.writeHead(200);
-          response.end(template);
+            var template = templateHTML(title, list, `<p>${description}</p>`);
+            response.writeHead(200);
+            response.end(template);
           });
         });
       }
